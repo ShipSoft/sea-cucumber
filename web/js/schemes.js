b2905b72e78361f54872f4942398349780659ec0
Katharina Albrecht <albrecka@physik.hu-berlin.de>
2026-09-14 00:35:16 +0200

    experimenting with color schemes: SHiP dark dark blue monochrome, SHiP light; adding comments on color assignments

--- web/js/schemes.js
+++ web/js/schemes.js
@@ -73,17 +73,17 @@ export const SCHEMES = {
     ship_db7: {
       // first color in geometry: calo + hall
       // second color: box around spectrometer magnet?
-      // third color: 
+      // third color: most of the spectrometer, some small plane in the calo
     label: "SHiP dark dark blue monochrome",
     bg: "#0a1528", bg2: "#081b3c", surface: "#12305f",
     text: "#d3dae8", accent: "#8fa1c5", hit: "#e8ecf3", vertex: "#79eba",
-    geometry: ["#20428a", "#365697", "#c64284", "#617aad",
+    geometry: ["#20428a", "#365697", "#4b67a2", "#c64284",
                "#798eba", "#8fa1c5"],
   },
   ship_light: {
     label: "SHiP light",
     bg: "#ffffff", bg2: "#c0c0c0", surface: "#bbbbbb",
-    text: "#232323", accent: "#e3a93c", hit: "#c64284", vertex: "#e3a0c1",
+    text: "#232323", accent: "#e3a93c", hit: "#081b3c", vertex: "#e3a0c1",
     geometry: ["#fafafa", "#f7f7f7", "#f3f3f3", "#eeeeee",
                "#eaeaea", "#e7e7e7", "#e3e3e3"],
   },
