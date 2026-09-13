887f536d50ea03ae9f5fbf750b457ed637e235b4
Katharina Albrecht <albrecka@physik.hu-berlin.de>
2026-09-14 00:57:20 +0200

    experimenting with color schemes: SHiP dark dark blue monochrome, SHiP light; adding comments on color assignments

--- web/js/schemes.js
+++ web/js/schemes.js
@@ -77,16 +77,17 @@ export const SCHEMES = {
       // forth color: spectrometer magnet itself? + UBT
       // fifth color: SBT support structure, plates in the spectrometer, muon shield
       // sixth color: some structures within UBT
+      // seventh color (additionally added): something at the spectrometer, target?
     label: "SHiP dark dark blue monochrome",
     bg: "#0a1528", bg2: "#081b3c", surface: "#12305f",
     text: "#d3dae8", accent: "#8fa1c5", hit: "#e8ecf3", vertex: "#79eba",
     geometry: ["#20428a", "#365697", "#4b67a2", "#61aad",
-               "#798eba", "#8fa1c5", "#c64284"],
+               "#798eba", "#8fa1c5", "#a5b3d0", "#c64284"],
   },
   ship_light: {
     label: "SHiP light",
     bg: "#ffffff", bg2: "#c0c0c0", surface: "#bbbbbb",
-    text: "#232323", accent: "#e3a93c", hit: "#081b3c", vertex: "#c64284",
+    text: "#232323", accent: "#e3a93c", hit: "#c64284", vertex: "#c64284",
     geometry: ["#fafafa", "#fbfbfb", "#fdfdfd", "#f6f6f6",
                "#fcfcfc", "#f5f5f5"],
   },
